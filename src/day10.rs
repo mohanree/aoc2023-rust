8d27e943ed6a35c3167e9162d221751d4331aff4
Uma Mohan Reemala <umamohan.reemala@gmail.com>
2023-12-11 01:27:19 -0500

    Push latest

--- src/day10.rs
+++ src/day10.rs
@@ -1,15 +1,31 @@
 /*
 */
 
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::Read;
+use lazy_static::lazy_static;
 
-fn start_loc(grid: &Vec<Vec<char>>) -> (i64, i64) {
+lazy_static! {
+    static ref PIPE_DIRS: HashMap<char, Vec<(isize, isize)>> = {
+        let mut m = HashMap::new();
+        m.insert('|', vec![(-1, 0), (1, 0)]);
+        m.insert('-', vec![(0, -1), (0, 1)]);
+        m.insert('L', vec![(-1, 0), (0, 1)]);
+        m.insert('J', vec![(-1, 0), (0, -1)]);
+        m.insert('7', vec![(0, -1), (1, 0)]);
+        m.insert('F', vec![(1, 0), (0, 1)]);
+        m.insert('.', vec![]);
+        m.insert('S', vec![(-1, 0), (0, -1), (1, 0), (0, 1)]);
+        m
+    };
+}
+
+fn start_loc(grid: &Vec<Vec<char>>) -> (usize, usize) {
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
             if grid[i][j] == 'S' {
-                return (i.try_into().unwrap(), j.try_into().unwrap());
+                return (i, j);
             }
         }
     }
@@ -17,7 +33,7 @@ fn start_loc(grid: &Vec<Vec<char>>) -> (i64, i64) {
     (0, 0)
 }
 
-fn is_valid(loc: (i64, i64), grid: &Vec<Vec<char>>) -> bool {
+fn is_valid(loc: (isize, isize), grid: &Vec<Vec<char>>) -> bool {
     if loc.0 < 0 || loc.0 >= grid.len().try_into().unwrap() {
         return false;
     }
@@ -31,32 +47,28 @@ fn is_valid(loc: (i64, i64), grid: &Vec<Vec<char>>) -> bool {
     true
 }
 
-fn dfs(
-    loc: (i64, i64),
-    grid: &Vec<Vec<char>>,
-    visited: &mut HashSet<(i64, i64)>,
-    dist: i64,
-) -> i64 {
-    if !is_valid(loc, grid) || visited.contains(&loc) {
-        return dist;
-    }
+fn bfs_walk ( loc: (usize, usize), grid: &Vec<Vec<char>>) {
 
-    visited.insert(loc);
-    println!("Loc -> {:?} {:?} {:?} ", loc, dist, visited);
+    let mut q: VecDeque <(usize, usize)> = VecDeque::new();
+    let mut visted = vec!(vec!(false;grid[0].len());grid.len());
+    q.push_back(loc);
+    visted[loc.0][loc.1] = true;
 
-    let mut max_dist = dist;
-    let dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)];
-    for (dx, dy) in dirs {
-        let n_loc = (loc.0 + dx, loc.1 + dy);
-        if is_valid(n_loc, grid)  {
-            max_dist = max_dist.max(dfs(n_loc, grid, visited, dist + 1))
+    while let Some(t) = q.pop_front() {
+        let p = grid[t.0][t.1];
+        println!("{:?} {:?}", t, q);
+        for (dx, dy) in PIPE_DIRS.get(&p).unwrap() {
+            let new_loc = (t.0 as isize + dx, t.1 as isize + dy);
+
+            if is_valid(new_loc, grid) && !visted[new_loc.0 as usize][new_loc.1 as usize] {
+                visted[new_loc.0 as usize][new_loc.0 as usize] = true;
+                q.push_back((new_loc.0 as usize, new_loc.0 as usize));
+            }
         }
     }
-
-    max_dist
 }
 
-fn process_input_lines(haystack: &str) -> i64 {
+fn process_input_lines(haystack: &str) -> isize {
     let grid = haystack
         .lines()
         .map(|line| line.chars().map(|c| c).collect::<Vec<char>>())
@@ -64,11 +76,20 @@ fn process_input_lines(haystack: &str) -> i64 {
 
     let s = start_loc(&grid);
 
-    let mut visited: HashSet<(i64, i64)> = HashSet::new();
-    let max_dist = dfs(s, &grid, &mut visited, 0);
+    let mut visited: HashSet<(isize, isize)> = HashSet::new();
+    let mut node2dist: HashMap<(isize, isize), isize> = HashMap::new();
+    bfs_walk(s, &grid);
 
     println!("Grid {:?} -> {:?}", grid, s);
-    max_dist
+    let mut u_grid: Vec<Vec<isize>>  = vec![vec![-1;grid[0].len()];grid.len()];
+    for (e, v) in node2dist.iter() {
+    //    u_grid[e.0 as isize][e.1 as isize] = *v;
+    }
+
+    println!("Node2Dist {:?}", node2dist);
+    println!("Ugrid {:?}", u_grid);
+
+    0
 }
 
 fn process_input_lines2(haystack: &str) -> u32 {
---
+++ src/day8.rs
@@ -0,0 +1,116 @@
+/*
+*/
+
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::Read;
+
+use regex::Regex;
+
+fn process_input_lines(haystack: &str) -> u32 {
+    let mut lines = haystack.lines();
+    let inst = lines.next().unwrap_or_default();
+    let re = Regex::new(r"^(\w+)\s*=\s*\((\w+),\s*(\w+)\)$").unwrap();
+    let mut data: HashMap<&str, (&str, &str)> = HashMap::new();
+
+    for line in lines {
+        if let Some(caps) = re.captures(line) {
+            data.insert(
+                caps.get(1).unwrap().as_str(),
+                (caps.get(2).unwrap().as_str(), caps.get(3).unwrap().as_str()),
+            );
+        }
+    }
+
+    let mut cursor = "AAA";
+    let mut counter = 0;
+
+    'hop: loop {
+        for c in inst.chars() {
+            if let Some(&(left, right)) = data.get(cursor) {
+                cursor = if c == 'R' { right } else { left };
+                counter += 1;
+                if cursor == "ZZZ" {
+                    break 'hop;
+                }
+            } else {
+                break 'hop;
+            }
+        }
+    }
+
+    counter
+}
+
+fn process_input_lines2(haystack: &str) -> u32 {
+    let mut lines = haystack.lines();
+    let inst = lines.next().unwrap_or_default();
+    let re = Regex::new(r"^(\w+)\s*=\s*\((\w+),\s*(\w+)\)$").unwrap();
+    let mut data: HashMap<&str, (&str, &str)> = HashMap::new();
+
+    let mut source_vec: Vec<&str> = vec![];
+    //let mut visited: HashSet<&str> = HashSet::new();
+    for line in lines {
+        if let Some(caps) = re.captures(line) {
+            let t = caps.get(1).unwrap().as_str();
+            if t.ends_with('A') {
+                source_vec.push(t);
+            }
+            data.insert(
+                t,
+                (caps.get(2).unwrap().as_str(), caps.get(3).unwrap().as_str()),
+            );
+        }
+    }
+
+    let mut counter = 0;
+
+    println!("Source vec: {:?}", source_vec);
+
+    let mut next_source_vec: Vec<&str> = Vec::with_capacity(source_vec.len());
+    'hop: loop {
+        for c in inst.chars() {
+            counter += 1;
+            let mut all_end_with_z = true;
+            next_source_vec.clear();
+            //println!("Source vec: {:?}", source_vec);
+            for &s in &source_vec {
+                if let Some(&(left, right)) = data.get(s) {
+                    let next_cursor = if c == 'R' { right } else { left };
+                    next_source_vec.push(next_cursor);
+                    if !next_cursor.ends_with('Z') {
+                        all_end_with_z = false;
+                    }
+                } else {
+                    break 'hop;
+                }
+            }
+            if all_end_with_z {
+                break 'hop;
+            } 
+            
+            //source_vec = next_source_vec;
+            std::mem::swap(&mut source_vec, &mut next_source_vec);            
+
+            if counter % 100000 == 0 {
+                print!("\r{:20} {:20} ", "Counting: ", counter);
+            } 
+        }
+    }
+    counter
+}
+
+pub fn play() {
+    let mut contents = String::new();
+
+    match File::open("data/d8_input.txt") {
+        Ok(mut file) => match file.read_to_string(&mut contents) {
+            Ok(_) => {
+                println!("Puzzle # 5.1: {}", process_input_lines(&contents));
+                println!("Puzzle # 5.2: {}", process_input_lines2(&contents));
+            }
+            Err(e) => println!("Error reading file: {}", e),
+        },
+        Err(e) => println!("Error opening file: {}", e),
+    }
+}
