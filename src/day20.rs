4befae5564b10599a25a9c82ac5c0a1bd1a23b6a
Mohan <umreemala@gmail.com>
2024-01-04 11:54:33 -0500

    Puzz 20#2 completed

--- src/day20.rs
+++ src/day20.rs
@@ -11,6 +11,7 @@ use std::io::Read;
 use std::rc::Rc;
 
 use crate::util::util;
+use num::integer::gcd;
 
 #[derive(Debug)]
 struct Module {
@@ -129,7 +130,129 @@ fn process_input_lines(input: &str) -> i64 {
     low * hi
 }
 
-fn process_input_lines2(input: &str) -> i64 {
+
+fn process_input_lines2(input: &str) -> usize {
+    let mut modules: HashMap<String, Module> = HashMap::new();
+
+    for line in input.lines() {
+        if let Some(m) = parse_module(line) {
+            modules.insert(m.name.clone(), m);
+        }
+    }
+
+    //println!("Modules {:?}", modules);
+    let mut updates = Vec::new();
+    for m in modules.values() {
+        for o in &m.out_names {
+            if let Some(t) = modules.get(o) {
+                if t.mod_type == '&' {
+                    updates.push((o.clone(), m.name.clone()));
+                }
+            }
+        }
+    }
+
+    for (o, m_name) in updates {
+        if let Some(t) = modules.get_mut(&o) {
+            t.memory.insert(m_name, false);
+        }
+    }
+
+    
+    //println!("Modules {:?}", modules);
+
+    let rx_feeders: Vec<_> = modules.iter().filter(|(_,m)|{
+        m.out_names.contains(&"rx".to_string())
+    }).map(|(e,_)|{
+        e
+    }).collect();
+
+    //println!("RX Feeder  = {:?}", rx_feeders);
+    let rx_feeder = rx_feeders.get(0).unwrap().to_string();
+
+    let rx_feeder_minus_1_visit_keys: Vec<_> = modules.iter().filter(|(_, m)| {
+        m.out_names.contains(&rx_feeder)
+    }).map(|(e, _)| e.clone()).collect();
+
+    let mut rx_feeder_minus_1_visit: HashMap<_, _> = rx_feeder_minus_1_visit_keys.into_iter().map(|e| (e, false)).collect();
+
+    let mut rx_feeder_cycles:HashMap<String, usize> = HashMap::new(); 
+    let mut low: i64 = 0;
+    let mut hi: i64 = 0;
+
+    let mut pulse_counter = 0 as usize;
+
+    'outer:for _ in 0..10000 {
+        pulse_counter += 1;
+        low += 1;
+        let mut q: VecDeque<(String, String, bool)> = VecDeque::new();
+        if let Some(b) = modules.get("broadcaster") {
+            for t in &b.out_names {
+                q.push_back((b.name.to_string(), t.to_string(), false));
+            }
+        }
+
+        //println!("Q {:?}", q );
+        while let Some((src, dest, pulse)) = q.pop_front() {
+            let pp = if pulse { "hi" } else { "lo" };
+            //println!("{:?} --{:?}-> {:?} ({:?}, {:?})", src, pp, dest, low, hi);
+            if pulse {
+                hi += 1
+            } else {
+                low += 1
+            }
+
+            match dest == rx_feeder && pulse {
+                true => { 
+                    let s = src.to_string();
+                    if let Some(value) = rx_feeder_minus_1_visit.get_mut(&s) {
+                        *value = true;
+                    }
+
+                    rx_feeder_cycles.entry(s).or_insert(pulse_counter);
+
+                   // println!("RX Feeder - 1 {:?} ", rx_feeder_minus_1_visit);
+                    //println!("RX Feeder Cycles- 1 {:?} ", rx_feeder_cycles);
+
+                    if rx_feeder_minus_1_visit.iter().all(|(_, visted)| { *visted } ) {
+                        let mut c: usize = 1;
+                        for t in rx_feeder_cycles.values() {
+                            c = c * (t/gcd(c, *t)) 
+                        }
+
+                        return c;
+
+                    }
+                }
+                false => (),
+            }
+
+            if let Some(dest_node) = modules.get_mut(&dest) {
+                if dest_node.mod_type == '%' {
+                    if !pulse {
+                        if let Some(s) = dest_node.memory.get("state") {
+                            let ns = !s;
+                            dest_node.memory.insert("state".to_string(), ns);
+
+                            for t in &dest_node.out_names {
+                                q.push_back((dest_node.name.to_string(), t.to_string(), ns));
+                            }
+                        }
+                    }
+                } else {
+                    dest_node.memory.insert(src, pulse);
+                    //println!("{:?}", dest_node);
+                    let np = !dest_node.memory.values().all(|p| *p);
+                    for t in &dest_node.out_names {
+                        q.push_back((dest_node.name.to_string(), t.to_string(), np));
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+
     0
 }
 
@@ -140,7 +263,7 @@ pub fn play() {
         Ok(mut file) => match file.read_to_string(&mut contents) {
             Ok(_) => {
                 println!("Puzzle # 19.1: {}", process_input_lines(&contents));
-                //println!("Puzzle # 18.2: {}", process_input_lines2(&contents));
+                println!("Puzzle # 19.2: {}", process_input_lines2(&contents));
             }
             Err(e) => println!("Error reading file: {}", e),
         },
